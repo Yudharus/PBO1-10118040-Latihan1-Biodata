976475e90595589b59ab41648bf758c7e7993a3d
Moch Yudha Rusdian <55824602+Yudharus@users.noreply.github.com>
2019-09-26 13:07:54 +0700

    Add files via upload

---
+++ PBO1-10118040-Latihan1-Biodata/src/biodata.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Moch Yudha Rusdian
+ * Kelas     : PBO1
+ * NIM       : 10118040
+ * Deskripsi : Menampilkan biodata
+ */
+public class biodata {
+    
+    public static void main(String[] args){
+        System.out.println("Biodat" +"\n"+ "----------------------------");
+        System.out.println("Nama\t\t:Moch Yudha Rusdian");
+        System.out.println("Alamat\t\t:Griya Bandung Asri 2");
+        System.out.println("Tempat Lahir\t:Lampung");
+        System.out.println("Tanggal Lahir\t:22 Juni 2000");
+        System.out.println("----------------------------");
+        System.out.println("Hobi\t\t:Tidur");
+        System.out.println("Makes\t\t:Mie");
+        System.out.println("Mikes\t\t:Jus Mangga");
+    }
+   
+}
